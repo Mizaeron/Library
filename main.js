3c9a879bca4876f6030660973cabfcf416952c45
Miza <mizaeron@gmail.com>
2024-10-26 18:02:14 +0100

    Create basic display for books

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+const myLibrary = ["theHobbit", "alchemist", "crimeAndPunishment"];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
-    };
 }
 
 const theHobbit = new Book('The Hobbit', 'by J.R.R Tolkien', '295 pages', 'not read yet');
-console.log(theHobbit.info());
+const alchemist = new Book('The Alchemist', 'By Paulo Coelho', '192 pages', 'read' );
+const crimeAndPunishment = new Book('Crime and Punishment', 'by Fjodor M. Dostojewskij', '528 pages', 'not read yet');
