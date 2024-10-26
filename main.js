262a7570c3eb21781eb82ec01ffd756842620416
Miza <mizaeron@gmail.com>
2024-10-26 18:22:15 +0100

    Add book info with DOM from array

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ function Book(title, author, pages, read) {
 const theHobbit = new Book('The Hobbit', 'by J.R.R Tolkien', '295 pages', 'not read yet');
 const alchemist = new Book('The Alchemist', 'By Paulo Coelho', '192 pages', 'read' );
 const crimeAndPunishment = new Book('Crime and Punishment', 'by Fjodor M. Dostojewskij', '528 pages', 'not read yet');
+
+const title = document.querySelector(".book .title");
+const author = document.querySelector(".book .author");
+const pages = document.querySelector(".book .pages");
+const read = document.querySelector(".book .read-status");
+
+title.textContent = theHobbit.title;
+author.textContent = theHobbit.author;
+pages.textContent = theHobbit.pages;
+read.textContent = theHobbit.read;
